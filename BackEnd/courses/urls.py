4874d491d8993a2767d84bdc248fa2fd1840d621
Samuel-Chi <131265603+Samuel-Chi@users.noreply.github.com>
2026-01-09 23:14:29 +0100

    Delete BackEnd directory
    clearing before committing my new code

--- BackEnd/courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import teacher_courses, delete_course, list_assignments, create_assignment
-
-urlpatterns = [
-    path('api/teacher-courses/', teacher_courses, name='teacher_courses'),
-    path('api/delete-course/<int:course_id>/', delete_course, name='delete_course'),
-    path('api/<int:course_id>/assignments/', list_assignments, name='list_assignments'),
-    path('api/<int:course_id>/assignments/create/', create_assignment, name='create_assignment')
-]
--- BackEnd/notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import notification_list, mark_as_read
-
-urlpatterns = [
-    path('', notification_list, name='notification_list'),
-    path('mark-read/<int:notification_id>/', mark_as_read, name='mark_as_read'),
-]
